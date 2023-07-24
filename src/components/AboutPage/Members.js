3dd9486fd80cf0b45034c43f89ca6d5a5fdded9a
csmanel <csmanel@gmail.com>
2023-07-24 12:24:51 -0400

    add addition member information

--- src/components/AboutPage/Members.js
+++ src/components/AboutPage/Members.js
@@ -1,5 +1,7 @@
 import cole from "../../assets/cole-headshot.png"
 import magdalena from "../../assets/magdalena-bio.jpg"
+import cameron from "../../assets/cameron-headshot.jpeg"
+import mira from "../../assets/mira-headshot.jpg"
 
 
 export const membersInfo = [
@@ -15,13 +17,13 @@ export const membersInfo = [
     },
     {
         name:'cameron',
-        bio: 'this is cameron bio',
-        graphic: 'this is cameron graphic',   
+        bio: 'Cameron Welke spends most of his time explaining to well-meaning strangers that the lute is, in fact, quite a different instrument from the flute. He brings a passionate curiosity and a deep creative drive to all manner of historical plucked instruments, which he plays with “expert technical dexterity, consummate phrasing and endearing expressivity” (Chestnut Hill Local). Past and current engagements include performances with the Washington Bach Consort, Tempesta di Mare, the Folger Consort, the Indianapolis Baroque Orchestra, Early Music City, and Hesperus. In 2022, he gave the first lute masterclasses to ever take place in the Dominican Republic through La Foundation de Conciertos de la Villa de Santo Domingo. He explores repertoire for two baroque lutes in Duo Silvio with duo partner Richard Stone; the two gave the modern premiere of Stone’s reconstructions of lute duets by Sylvius Leopold Weiss in the fall of 2019. He holds a B.M. in classical guitar performance from Belmont University, where he studied with Francis Perry and John Pell, and a M.M. in historical performance on lute and theorbo from the Peabody Conservatory, where he studied with Richard Stone. He currently teaches lute at Grinnell College and lives in Richmond, VA.',
+        graphic: cameron,   
     },
     {
         name:'mira',
-        bio: 'this is mira bio',
-        graphic: 'this is mira graphic',   
+        bio: "Soprano Mira Fu-En Huang uses her voice to veer off the beaten path and bring lesser-told stories to the forefront. Her repertory sprawls across cultures and eras, from folk music to medieval motets and the works of living composers. Hailed for her “nuanced vocal splendor” and “mesmerizing style” (Upstage), Mira especially enjoys bringing life to underrepresented stories from early and modern eras. She has performed as Semele in Handel’s opera of the same name, Cupid in Blow’s Venus and Adonis, Night in Purcell’s Fairy Queen, Euridice in Monteverdi’s L’Orfeo, and Belinda in Purcell’s Dido and Aeneas. She also enjoys collaborations with living composers across the globe. Mira obtained an M.M. in Historical Performance from the Peabody Institute of the Johns Hopkins University, studying under Ah Young Hong. She also received undergraduate degrees in Vocal Performance and Psychology, as well as a minor in English, from the University of California, Davis. She has been coached by a variety of early and new music specialists, including Quince Ensemble, Roomful of Teeth, Dame Emma Kirkby, and Julianne Baird. Currently, Mira works as a freelance soprano and full-time administrator in Maryland. She enjoys researching, reading, and writing in numerous genres. In her free time, she can be found playing flute and viola da gamba, drinking excessive amounts of coffee, and writing in various book-filled locales.",
+        graphic: mira,   
     },
     {
         name:'liv',
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,21 @@
 export const Contact = () => {
-    return <div>contact page</div>;
+    return( 
+    
+    <div>
+      <h1>contact form</h1>
+      <form>
+        <label>name</label>
+        <input type='text' name='name' />
+
+        <label>email</label>
+        <input type='email' name='user_email'/>
+
+        <label>message</label>
+        <textarea name='message' rows='4' />
+        <input type='submit'  value='send'/>
+        
+      </form>
+    </div>
+    )
   };
   
