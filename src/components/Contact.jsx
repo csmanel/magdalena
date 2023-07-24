0238fab7b23b735200d9a9c0995cd30f45150212
csmanel <csmanel@gmail.com>
2023-07-24 12:56:52 -0400

    add emailjs functionality

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,21 +1,122 @@
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
+
 export const Contact = () => {
-    return( 
-    
-    <div>
-      <h1>contact form</h1>
-      <form>
-        <label>name</label>
-        <input type='text' name='name' />
+  const form = useRef();
 
-        <label>email</label>
-        <input type='email' name='user_email'/>
+  const sendEmail = (e) => {
+    e.preventDefault();
 
-        <label>message</label>
-        <textarea name='message' rows='4' />
-        <input type='submit'  value='send'/>
-        
+    emailjs.sendForm('service_spqgqxn', 'template_hxpzojx', form.current, 'qzHs9MbBDYRSgPExM')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };  
+  
+  return (
+    <>
+      <form ref={form} onSubmit={sendEmail}>
+        <label>Name</label>
+        <input type="text" name="user_name" />
+        <label>Email</label>
+        <input type="email" name="user_email" />
+        <label>Message</label>
+        <textarea name="message" />
+        <input type="submit" value="Send" />
       </form>
-    </div>
-    )
-  };
+    </>
+  );
+};
+
+export default Contact;
+
+// Styles
+// const StyledContactForm = styled.div`
+//   width: 400px;
+
+//   form {
+//     display: flex;
+//     align-items: flex-start;
+//     flex-direction: column;
+//     width: 100%;
+//     font-size: 16px;
+
+//     input {
+//       width: 100%;
+//       height: 35px;
+//       padding: 7px;
+//       outline: none;
+//       border-radius: 5px;
+//       border: 1px solid rgb(220, 220, 220);
+
+//       &:focus {
+//         border: 2px solid rgba(0, 206, 158, 1);
+//       }
+//     }
+
+//     textarea {
+//       max-width: 100%;
+//       min-width: 100%;
+//       width: 100%;
+//       max-height: 100px;
+//       min-height: 100px;
+//       padding: 7px;
+//       outline: none;
+//       border-radius: 5px;
+//       border: 1px solid rgb(220, 220, 220);
+
+//       &:focus {
+//         border: 2px solid rgba(0, 206, 158, 1);
+//       }
+//     }
+
+//     label {
+//       margin-top: 1rem;
+//     }
+
+//     input[type="submit"] {
+//       margin-top: 2rem;
+//       cursor: pointer;
+//       background: rgb(249, 105, 14);
+//       color: white;
+//       border: none;
+//     }
+//   }
+// `;
+
+  // return( 
+  //   <div>
+  //     <form ref={form} onSubmit={sendEmail}>
+  //     <label>Name</label>
+  //     <input type="text" name="user_name" />
+  //     <label>Email</label>
+  //     <input type="email" name="user_email" />
+  //     <label>Message</label>
+  //     <textarea name="message" />
+  //     <input type="submit" value="Send" />
+  //     </form>
+      
+  //     <div>
+  //     {/* <h1>contact form</h1>
+  //       <form>
+  //         <label>name</label>
+  //         <input type='text' name='name' />
+
+  //         <label>email</label>
+  //         <input type='email' name='user_email'/>
+
+  //         <label>message</label>
+  //         <textarea name='message' rows='4' />
+  //         <input type='submit'  value='send'/>
+        
+  //       </form> */}
+  //     </div>
+  //   </div>
+  //   )
+  // };
+  
+
   
