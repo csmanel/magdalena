b456cd734d6912d8643d7e0b053ef57a9a656cef
Cole Manel <csmanel@gmail.com>
2024-06-24 15:33:40 -0400

    remove navlinks when mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,51 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { NavLink } from 'react-router-dom';
+import { FaBars } from 'react-icons/fa';
 
 export const Navbar = () => {
+  const [isClicked, setIsClicked] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+
+  const toggleNav = () => {
+    setIsClicked(!isClicked);
+  };
+
+  const handleResize = () => {
+    setIsMobile(window.innerHeight <= 768);
+  };
+  useEffect(() => {
+    handleResize();
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
     <nav className="primary-nav">
-      <NavLink to="/" >
-        home
-      </NavLink>
+      {isMobile && (
+        <button className="nav-toggle" onClick={toggleNav}>
+          <FaBars />
+          {console.log(isClicked)}
+        </button>
+      )}
 
-      <NavLink to="/events" >
-        events
-      </NavLink>
-
-      <NavLink to="/about" >
-        about
-      </NavLink>
-
-      <NavLink to="/contact" >
-        contact
-      </NavLink>
-
-      <NavLink to="/support" >
-        support
-      </NavLink>
+      <div className={`nav ${isClicked ? 'clicked' : ''}`}>
+        <NavLink to="/" onClick={() => setIsClicked(false)}>
+          home
+        </NavLink>
+        <NavLink to="/events" onClick={() => setIsClicked(false)}>
+          events
+        </NavLink>
+        <NavLink to="/about" onClick={() => setIsClicked(false)}>
+          about
+        </NavLink>
+        <NavLink to="/contact" onClick={() => setIsClicked(false)}>
+          contact
+        </NavLink>
+        <NavLink to="/support" onClick={() => setIsClicked(false)}>
+          support
+        </NavLink>
+      </div>
     </nav>
   );
 };
