0b84bcd1833cf8090fd3bd90307e79909c57f66b
Cole Manel <csmanel@gmail.com>
2024-11-08 18:13:57 -0500

    fix hover state on buttons

--- src/components/Buttons/CarouselButtons/CarouselButton.jsx
+++ src/components/Buttons/CarouselButtons/CarouselButton.jsx
@@ -1,12 +1,16 @@
 const CarouselButton = ({ label, onClick, disabled }) => (
+  // here we can use conditional logic to handle disabled states
+  // in tailwind, if there are disabled attributes the hover styling won't apply
   <button
     onClick={onClick}
     disabled={disabled}
-    className={`inline-flex items-center justify-center bg-[#ffe8cd] rounded-[10px] text-md font-bold 
-              m-[20px] w-[150px] h-[30px] border-none cursor-pointer select-none 
-              hover:border-2 hover:border-[#222] 
-              active:bg-[#222] active:text-white 
-              disabled:bg-[#cccccc] disabled:border disabled:border-[#999999] disabled:text-[#666666] disabled:cursor-not-allowed`}
+    className={`inline-flex items-center justify-center rounded-[10px] text-md font-bold 
+              m-[20px] w-[150px] h-[30px] cursor-pointer select-none 
+              ${
+                disabled
+                  ? 'bg-[#cccccc] border border-[#999999] text-[#666666] cursor-not-allowed'
+                  : 'bg-[#ffe8cd] hover:border-2 hover:border-[#222] active:bg-[#222] active:text-white'
+              }`}
   >
     {label}
   </button>
