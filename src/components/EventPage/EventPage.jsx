bdb720311758e94665b77e2b1d8b4063a5277330
csmanel <csmanel@gmail.com>
2024-11-22 19:44:52 -0500

    font breakpoint on screen 1280px

--- src/components/EventPage/EventPage.jsx
+++ src/components/EventPage/EventPage.jsx
@@ -15,7 +15,7 @@ export const EventPage = () => {
   }
 
   return (
-    <div className="flex flex-col items-center justify-center bg-[#bd7e4a] bg-opacity-95 shadow-lg rounded-lg mt-32 pt-10 pr-10 pb-5 pl-10  max-w-[1000px] xl:max-h-[500px] xl:mt-16">
+    <div className="flex flex-col items-center justify-center bg-[#bd7e4a] bg-opacity-95 shadow-lg rounded-lg mt-32 pt-10 pr-10 pb-5 pl-10  max-w-[1000px] xl:max-h-[500px] xl:mt-16 xl:pt-5">
       <EventCard step={step} />
 
       <div className="flex pt-0">
